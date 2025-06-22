4fd8d2b52b16ee3fc0fbd37f40d3b136fb81099e
Duarte Gonçalves <duartesousagoncalves@gmail.com>
2025-06-22 11:45:13 +0100

    Generate individual available/2 predicates for Prolog

--- data_pl/availability.pl
+++ data_pl/availability.pl
@@ -1,34 +1,229 @@
-availability([
-    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],  % André Lima
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],  % António Santos
-    [0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0],  % Beatriz Ferreira
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  % Bruno Oliveira
-    [0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  % Clara
-    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0],  % Diogo Fernandes
-    [0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  % Eduarda Magno
-    [1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],  % Francisco Cardoso
-    [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1],  % Goiana
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],  % Guilherme Santos
-    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1],  % Inês Gaspar
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  % José Martins
-    [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],  % José Sousa
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],  % João Silva
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],  % João Torre Pereira
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  % Luís Sousa
-    [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1],  % Madalena
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],  % Marco Vilas Boas
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  % Maria Rita Nogueira Lopes
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0],  % Martim Moniz
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  % Miguel Garrido
-    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],  % Miguel Moita
-    [0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0],  % Nery
-    [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],  % Pedro Vidal
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  % Rita Cachaldora
-    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0],  % Rubem Neto
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],  % Ruben Esteves
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  % Rui Silveira
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1],  % Solange
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],  % Tomás Figueiredo Marques Palma
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1],  % Tomás Ramos
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0]  % Vítor Pires
-]).
+available('André Lima', workshop_react_next_1).
+available('André Lima', escape_room_1).
+available('André Lima', workshop_react_next_2).
+available('André Lima', coffee_break_1).
+available('André Lima', talk_design_ai).
+available('André Lima', workshop_github_actions).
+available('António Santos', escape_room_2).
+available('António Santos', escape_room_3).
+available('António Santos', talk_chess).
+available('António Santos', workshop_github_actions).
+available('Beatriz Ferreira', coffee_break_1).
+available('Beatriz Ferreira', escape_room_2).
+available('Beatriz Ferreira', escape_room_3).
+available('Beatriz Ferreira', talk_visual_design).
+available('Beatriz Ferreira', escape_room_4).
+available('Beatriz Ferreira', escape_room_5).
+available('Beatriz Ferreira', escape_room_6).
+available('Beatriz Ferreira', escape_room_7).
+available('Beatriz Ferreira', talk_design_ai).
+available('Bruno Oliveira', escape_room_1).
+available('Bruno Oliveira', workshop_react_next_2).
+available('Bruno Oliveira', coffee_break_1).
+available('Bruno Oliveira', escape_room_2).
+available('Bruno Oliveira', escape_room_3).
+available('Bruno Oliveira', talk_visual_design).
+available('Bruno Oliveira', panel_pi).
+available('Bruno Oliveira', talk_haskell).
+available('Bruno Oliveira', escape_room_4).
+available('Bruno Oliveira', escape_room_5).
+available('Bruno Oliveira', escape_room_6).
+available('Bruno Oliveira', escape_room_7).
+available('Bruno Oliveira', coffee_break_2).
+available('Bruno Oliveira', talk_chess).
+available('Bruno Oliveira', talk_design_ai).
+available('Bruno Oliveira', workshop_github_actions).
+available('Bruno Oliveira', coffee_break_3).
+available('Clara', escape_room_1).
+available('Clara', workshop_react_next_2).
+available('Clara', coffee_break_1).
+available('Clara', escape_room_2).
+available('Clara', escape_room_3).
+available('Clara', talk_haskell).
+available('Diogo Fernandes', workshop_react_next_1).
+available('Diogo Fernandes', escape_room_1).
+available('Diogo Fernandes', talk_visual_design).
+available('Diogo Fernandes', panel_pi).
+available('Diogo Fernandes', talk_haskell).
+available('Diogo Fernandes', escape_room_4).
+available('Diogo Fernandes', escape_room_6).
+available('Diogo Fernandes', escape_room_7).
+available('Diogo Fernandes', coffee_break_2).
+available('Diogo Fernandes', talk_design_ai).
+available('Eduarda Magno', escape_room_1).
+available('Eduarda Magno', coffee_break_1).
+available('Eduarda Magno', escape_room_2).
+available('Eduarda Magno', escape_room_3).
+available('Eduarda Magno', escape_room_4).
+available('Eduarda Magno', escape_room_5).
+available('Francisco Cardoso', workshop_react_next_1).
+available('Francisco Cardoso', escape_room_1).
+available('Francisco Cardoso', talk_visual_design).
+available('Francisco Cardoso', talk_haskell).
+available('Francisco Cardoso', escape_room_4).
+available('Francisco Cardoso', escape_room_5).
+available('Francisco Cardoso', escape_room_6).
+available('Francisco Cardoso', escape_room_7).
+available('Francisco Cardoso', talk_chess).
+available('Francisco Cardoso', talk_design_ai).
+available('Francisco Cardoso', workshop_github_actions).
+available('Francisco Cardoso', coffee_break_3).
+available('Goiana', escape_room_1).
+available('Goiana', talk_visual_design).
+available('Goiana', escape_room_5).
+available('Goiana', escape_room_6).
+available('Goiana', escape_room_7).
+available('Goiana', talk_chess).
+available('Goiana', talk_design_ai).
+available('Goiana', workshop_github_actions).
+available('Goiana', coffee_break_3).
+available('Guilherme Santos', talk_visual_design).
+available('Guilherme Santos', panel_pi).
+available('Guilherme Santos', talk_haskell).
+available('Guilherme Santos', escape_room_6).
+available('Guilherme Santos', escape_room_7).
+available('Guilherme Santos', coffee_break_2).
+available('Guilherme Santos', talk_chess).
+available('Guilherme Santos', talk_design_ai).
+available('Guilherme Santos', workshop_github_actions).
+available('Guilherme Santos', coffee_break_3).
+available('Inês Gaspar', workshop_react_next_2).
+available('Inês Gaspar', coffee_break_1).
+available('Inês Gaspar', talk_chess).
+available('Inês Gaspar', workshop_github_actions).
+available('Inês Gaspar', coffee_break_3).
+available('José Sousa', workshop_react_next_2).
+available('José Sousa', coffee_break_1).
+available('José Sousa', escape_room_2).
+available('José Sousa', escape_room_3).
+available('José Sousa', talk_visual_design).
+available('José Sousa', escape_room_6).
+available('José Sousa', talk_chess).
+available('José Sousa', talk_design_ai).
+available('José Sousa', workshop_github_actions).
+available('José Sousa', coffee_break_3).
+available('João Silva', workshop_react_next_1).
+available('João Silva', talk_chess).
+available('João Silva', talk_design_ai).
+available('João Silva', coffee_break_3).
+available('João Torre Pereira', talk_design_ai).
+available('Luís Sousa', talk_visual_design).
+available('Luís Sousa', panel_pi).
+available('Luís Sousa', talk_haskell).
+available('Luís Sousa', escape_room_4).
+available('Luís Sousa', escape_room_5).
+available('Luís Sousa', escape_room_6).
+available('Luís Sousa', escape_room_7).
+available('Luís Sousa', coffee_break_2).
+available('Luís Sousa', talk_chess).
+available('Luís Sousa', talk_design_ai).
+available('Luís Sousa', workshop_github_actions).
+available('Luís Sousa', coffee_break_3).
+available('Madalena', workshop_react_next_1).
+available('Madalena', escape_room_1).
+available('Madalena', talk_visual_design).
+available('Madalena', escape_room_6).
+available('Madalena', escape_room_7).
+available('Madalena', talk_chess).
+available('Madalena', workshop_github_actions).
+available('Madalena', coffee_break_3).
+available('Marco Vilas Boas', talk_haskell).
+available('Marco Vilas Boas', talk_chess).
+available('Marco Vilas Boas', talk_design_ai).
+available('Marco Vilas Boas', workshop_github_actions).
+available('Marco Vilas Boas', coffee_break_3).
+available('Maria Rita Nogueira Lopes', workshop_react_next_1).
+available('Maria Rita Nogueira Lopes', talk_haskell).
+available('Martim Moniz', workshop_react_next_2).
+available('Martim Moniz', coffee_break_1).
+available('Martim Moniz', escape_room_2).
+available('Martim Moniz', escape_room_3).
+available('Martim Moniz', talk_visual_design).
+available('Martim Moniz', panel_pi).
+available('Martim Moniz', talk_haskell).
+available('Martim Moniz', escape_room_6).
+available('Martim Moniz', escape_room_7).
+available('Martim Moniz', coffee_break_2).
+available('Miguel Moita', talk_visual_design).
+available('Miguel Moita', panel_pi).
+available('Miguel Moita', talk_haskell).
+available('Miguel Moita', escape_room_6).
+available('Miguel Moita', escape_room_7).
+available('Miguel Moita', coffee_break_2).
+available('Miguel Moita', talk_chess).
+available('Miguel Moita', talk_design_ai).
+available('Miguel Moita', workshop_github_actions).
+available('Miguel Moita', coffee_break_3).
+available('Nery', workshop_react_next_2).
+available('Nery', coffee_break_1).
+available('Nery', panel_pi).
+available('Nery', talk_haskell).
+available('Nery', escape_room_7).
+available('Nery', coffee_break_2).
+available('Nery', talk_chess).
+available('Nery', workshop_github_actions).
+available('Pedro Vidal', workshop_react_next_1).
+available('Pedro Vidal', escape_room_1).
+available('Pedro Vidal', workshop_react_next_2).
+available('Pedro Vidal', coffee_break_1).
+available('Pedro Vidal', escape_room_2).
+available('Pedro Vidal', escape_room_3).
+available('Pedro Vidal', talk_chess).
+available('Pedro Vidal', talk_design_ai).
+available('Pedro Vidal', workshop_github_actions).
+available('Pedro Vidal', coffee_break_3).
+available('Rita Cachaldora', escape_room_1).
+available('Rita Cachaldora', workshop_react_next_2).
+available('Rita Cachaldora', coffee_break_1).
+available('Rita Cachaldora', escape_room_2).
+available('Rita Cachaldora', escape_room_3).
+available('Rita Cachaldora', talk_visual_design).
+available('Rita Cachaldora', panel_pi).
+available('Rita Cachaldora', talk_haskell).
+available('Rita Cachaldora', escape_room_4).
+available('Rita Cachaldora', escape_room_5).
+available('Rita Cachaldora', escape_room_6).
+available('Rita Cachaldora', escape_room_7).
+available('Rita Cachaldora', coffee_break_2).
+available('Rita Cachaldora', talk_chess).
+available('Rita Cachaldora', talk_design_ai).
+available('Rita Cachaldora', workshop_github_actions).
+available('Rita Cachaldora', coffee_break_3).
+available('Rubem Neto', workshop_react_next_2).
+available('Rubem Neto', coffee_break_1).
+available('Rubem Neto', escape_room_2).
+available('Rubem Neto', escape_room_3).
+available('Rubem Neto', talk_visual_design).
+available('Rubem Neto', panel_pi).
+available('Rubem Neto', talk_haskell).
+available('Rubem Neto', escape_room_6).
+available('Rubem Neto', escape_room_7).
+available('Rubem Neto', coffee_break_2).
+available('Ruben Esteves', workshop_react_next_1).
+available('Ruben Esteves', talk_chess).
+available('Ruben Esteves', talk_design_ai).
+available('Ruben Esteves', workshop_github_actions).
+available('Ruben Esteves', coffee_break_3).
+available('Solange', panel_pi).
+available('Solange', talk_haskell).
+available('Solange', talk_chess).
+available('Solange', talk_design_ai).
+available('Solange', workshop_github_actions).
+available('Solange', coffee_break_3).
+available('Tomás Figueiredo Marques Palma', workshop_react_next_1).
+available('Tomás Figueiredo Marques Palma', talk_chess).
+available('Tomás Figueiredo Marques Palma', talk_design_ai).
+available('Tomás Figueiredo Marques Palma', workshop_github_actions).
+available('Tomás Figueiredo Marques Palma', coffee_break_3).
+available('Tomás Ramos', talk_visual_design).
+available('Tomás Ramos', escape_room_4).
+available('Tomás Ramos', escape_room_5).
+available('Tomás Ramos', escape_room_6).
+available('Tomás Ramos', escape_room_7).
+available('Tomás Ramos', talk_chess).
+available('Tomás Ramos', talk_design_ai).
+available('Tomás Ramos', workshop_github_actions).
+available('Tomás Ramos', coffee_break_3).
+available('Vítor Pires', talk_visual_design).
+available('Vítor Pires', escape_room_6).
+available('Vítor Pires', escape_room_7).
--- scripts/data_import/availability.py
+++ scripts/data_import/availability.py
@@ -7,6 +7,7 @@ from datetime import datetime
 # The base date and time to calculate relative time blocks from
 BASE_DATETIME = datetime(2024, 10, 7, 0, 0, 0)
 
+# The event ID from the Lettuce Meet URL (e.g., https://lettucemeet.com/N6nEo)
 LETTUCE_MEET_EVENT_ID = 'N6nEo'
 
 def fetch_lettuce_meet_availability(event_id):
@@ -35,6 +36,7 @@ def fetch_lettuce_meet_availability(event_id):
         'sec-fetch-site': 'same-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     }
+    # This is the GraphQL query to get event details and poll responses.
     json_data = {
         'id': 'EventQuery',
         'query': 'query EventQuery(\n  $id: ID!\n) {\n  event(id: $id) {\n    ...Event_event\n    id\n  }\n}\n\nfragment Event_event on Event {\n  id\n  pollResponses {\n    user {\n      __typename\n      ... on AnonymousUser {\n        name\n      }\n      ... on User {\n        name\n      }\n    }\n    availabilities {\n      start\n      end\n    }\n  }\n}',
@@ -43,22 +45,24 @@ def fetch_lettuce_meet_availability(event_id):
     
     try:
         response = requests.post('https://api.lettucemeet.com/graphql', headers=headers, json=json_data)
-        response.raise_for_status()  # Raise an exception for bad status codes
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
         response_json = response.json()
         
         prolog_predicates = []
         jsondata = response_json.get("data", {}).get("event", {}).get("pollResponses", [])
 
         for i in jsondata:
-            # Capitalize the first letter of each word in the name
+            # Capitalize the first letter of each word in the name for consistency.
             user_name = i.get("user", {}).get("name", "Unknown").title()
             
             if "availabilities" in i and i["availabilities"]:
                 intervals_for_user = []
                 for availability in i["availabilities"]:
+                    # Convert ISO 8601 datetime strings to datetime objects.
                     start_dt = datetime.fromisoformat(availability["start"].replace('Z', ''))
                     end_dt = datetime.fromisoformat(availability["end"].replace('Z', ''))
                     
+                    # Calculate the start and end blocks in 15-minute increments from the base datetime.
                     start_block = int((start_dt - BASE_DATETIME).total_seconds() / (15 * 60))
                     end_block = int((end_dt - BASE_DATETIME).total_seconds() / (15 * 60))
                     
@@ -66,6 +70,7 @@ def fetch_lettuce_meet_availability(event_id):
                 
                 if intervals_for_user:
                     prolog_intervals = ", ".join(intervals_for_user)
+                    # Create the raw availability fact for the user.
                     prolog_predicates.append(f"availability('{user_name}', [{prolog_intervals}]).")
         
         print("Successfully fetched and processed data.")
@@ -81,7 +86,8 @@ def fetch_lettuce_meet_availability(event_id):
 
 def parse_availability(data):
     """
-    Parses the availability data strings into a dictionary.
+    Parses the availability data strings from fetch_lettuce_meet_availability
+    into a dictionary mapping names to their time intervals.
     """
     staff_availability = {}
     pattern = re.compile(r"availability\('([^']*)',\s*\[(.*)\]\)\.")
@@ -91,6 +97,7 @@ def parse_availability(data):
             name, intervals_str = match.groups()
             intervals = []
             if intervals_str:
+                # Find all start-end pairs in the interval string.
                 interval_pairs = re.findall(r'(\d+)-(\d+)', intervals_str)
                 for start, end in interval_pairs:
                     intervals.append([int(start), int(end)])
@@ -99,9 +106,10 @@ def parse_availability(data):
 
 def parse_activities(data):
     """
-    Parses the activity data strings into a list of dictionaries.
+    Parses the activity data strings from a Prolog file into a list of dictionaries.
     """
     activities = []
+    # Extracts slug, start time, and duration from an activity/4 predicate.
     pattern = re.compile(r"activity\(([^,]+),\s*\d+,\s*(\d+),\s*(\d+),.*\)\.")
     for line in data.strip().split('\n'):
         match = pattern.match(line)
@@ -112,39 +120,45 @@ def parse_activities(data):
             activities.append({
                 'slug': slug.strip(),
                 'start': start_time,
-                'end': start_time + duration
+                'end': start_time + duration  # Calculate the end time block.
             })
     return activities
 
 def check_availability(staff_intervals, activity):
     """
     Checks if a staff member is available for a given activity.
+    An availability of 1 means available, 0 means not available.
     """
     for interval in staff_intervals:
+        # Check if any of the staff's available intervals fully contains the activity's time window.
         if interval[0] <= activity['start'] and interval[1] >= activity['end']:
             return 1
     return 0
 
-def generate_prolog_predicate(staff_data, activity_data):
+def generate_availability_predicates(staff_data, activity_data):
     """
-    Generates the full Prolog availability/1 predicate as a string.
+    Generates individual Prolog 'available/2' predicates for each staff member
+    who is available for a specific activity.
     """
+    predicates = []
     staff_names = sorted(staff_data.keys())
-    output_lines = ["availability(["]
-    for i, name in enumerate(staff_names):
+    
+    # Iterate through each staff member and each activity
+    for name in staff_names:
         intervals = staff_data.get(name, [])
-        row = [check_availability(intervals, act) for act in activity_data]
-        row_str = f"    [{', '.join(map(str, row))}]"
-        comment = f"  % {name}"
-        if i < len(staff_names) - 1:
-            row_str += ','
-        output_lines.append(f"{row_str}{comment}")
-    output_lines.append("]).")
-    return "\n".join(output_lines)
+        for activity in activity_data:
+            # If the staff member is available, create a predicate.
+            if check_availability(intervals, activity) == 1:
+                activity_slug = activity['slug']
+                # Format: available('Staff Name', activity_slug).
+                predicates.append(f"available('{name}', {activity_slug}).")
+                
+    return "\n".join(predicates)
 
 def generate_json_output(staff_data, activity_data):
     """
     Generates the availability data as a list of dictionaries for JSON output.
+    This creates a matrix-like structure within the JSON file.
     """
     staff_names = sorted(staff_data.keys())
     activity_slugs = [act['slug'] for act in activity_data]
@@ -152,6 +166,7 @@ def generate_json_output(staff_data, activity_data):
     json_result = []
     for name in staff_names:
         intervals = staff_data.get(name, [])
+        # Create a row of 1s and 0s for the availability matrix.
         availability_row = [check_availability(intervals, act) for act in activity_data]
         
         staff_entry = {
@@ -166,12 +181,6 @@ def generate_json_output(staff_data, activity_data):
 def read_activities_from_file(filepath):
     """
     Reads activity data from a Prolog file.
-
-    Args:
-        filepath (str): The path to the activities Prolog file.
-
-    Returns:
-        str: The content of the file, or an empty string if not found.
     """
     print(f"Reading activities from {filepath}...")
     try:
@@ -182,21 +191,28 @@ def read_activities_from_file(filepath):
         return ""
 
 if __name__ == "__main__":
+    # Define file paths for input and output.
     activities_filepath = 'data_pl/activities.pl'
     prolog_availability_filepath = 'data_pl/availability.pl'
     json_availability_filepath = 'data_json/availability.json'
     
+    # Step 1: Fetch raw availability data from Lettuce Meet.
     staff_data_str = fetch_lettuce_meet_availability(LETTUCE_MEET_EVENT_ID)
     
+    # Step 2: Read activity definitions from a local file.
     activity_data_str = read_activities_from_file(activities_filepath)
 
+    # Proceed only if both data sources are available.
     if staff_data_str and activity_data_str:
+        # Step 3: Parse the raw strings into structured Python objects.
         parsed_staff = parse_availability(staff_data_str)
         parsed_activities = parse_activities(activity_data_str)
 
-        prolog_output = generate_prolog_predicate(parsed_staff, parsed_activities)
+        # Step 4: Generate the desired Prolog and JSON outputs.
+        prolog_output = generate_availability_predicates(parsed_staff, parsed_activities)
         json_output = generate_json_output(parsed_staff, parsed_activities)
 
+        # Step 5: Write the generated content to files.
         os.makedirs('data_pl', exist_ok=True)
         os.makedirs('data_json', exist_ok=True)
 
